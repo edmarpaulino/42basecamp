2455f5d2159f214098a7b2367b5dd6af3054593b
edpaulin <edpaulin@student.42sp.org.br>
2021-06-06 21:05:08 +0000

    conslusion main function

--- rush00/ex00/main.c
+++ rush00/ex00/main.c
@@ -1,22 +1,50 @@
-#include <stdlib.h>
-void	rush(int	x, int	y);
-void	ft_putchar(char	c);
+void	rush	(int	x, int	y);
+int		convert	(char	*parameter);
 
 int	main(int	argc, char	*argv[])
 {
-	int	row;
 	int	col;
+	int	row;
 
 	if (argc != 3)
 	{
-		row = 5;
 		col = 5;
+		row = 5;
 	}
 	else
 	{
-		row = atoi(argv[1]);
-		col = atoi(argv[2]);
+		col = convert(argv[1]);
+		row = convert(argv[2]);
 	}
-	rush(col, row);
+	if (col == 0 || row == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		rush(col, row);
+	}
 	return (0);
+}
+
+int	convert(char	*parameter)
+{
+	int	i;
+	int	converted_num;
+
+	i = 0;
+	converted_num = 0;
+	while (parameter[i] != '\0')
+	{
+		if (parameter[i] < '0' || parameter[i] > '9')
+		{
+			return (0);
+		}
+		else
+		{
+			converted_num = (converted_num * 10) + (parameter[i] - '0');
+			i++;
+		}
+	}
+	return (converted_num);
 }
